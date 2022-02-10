1c9dce3d08cbf4311562ce109804c43e63614faf
CodeDiary18 <codediary18@gmail.com>
2022-02-10 21:23:25 +0900

    Response JsonProperty 관련 설정

--- src/main/java/com/example/mail/dto/Res.java
+++ src/main/java/com/example/mail/dto/Res.java
@@ -1,5 +1,7 @@
 package com.example.mail.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,7 +9,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "message"})
 public class Res {
+    @JsonProperty("isSuccess")
     private boolean isSuccess;
     private String message;
 }
